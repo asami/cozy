a8f4b32a1b37e042d488fc1f6721c708326e1d16
Asami, Tomoharu <asami@asamioffice.com>
2026-03-17 04:23:01 +0900

    Add cli execution.

--- src/main/scala/cozy/Cozy.scala
+++ src/main/scala/cozy/Cozy.scala
@@ -4,6 +4,7 @@ import org.goldenport.RAISE
 import org.goldenport.i18n.I18NString
 import org.goldenport.cli.{Config => CliConfig, _}
 import org.goldenport.value._
+import org.goldenport.parser.CommandParser
 import org.goldenport.kaleidox.Kaleidox
 import org.goldenport.kaleidox.http.HttpHandle
 import org.smartdox.service.operations.HtmlOperationClass
@@ -14,7 +15,8 @@ import cozy.web.jetty.JettyServer
  *  version Dec. 19, 2021
  *  version Jan.  1, 2022
  *  version Feb. 28, 2022
- * @version Aug. 20, 2025
+ *  version Aug. 20, 2025
+ * @version Mar. 17, 2026
  * @author  ASAMI, Tomoharu
  */
 class Cozy(
@@ -28,14 +30,13 @@ class Cozy(
   def execute(args: Array[String]) = _engine.apply(environment, args)
 
   def run(args: Array[String]) {
-    val req = spec.Request.empty
-    val res = spec.Response()
-    val op = spec.Operation("cozy", req, res)
-    val call = OperationCall.create(op, args)
+    val call = _operation_call(args)
     if (call.request.arguments.isEmpty || call.request.isInteractive)
       repl(call)
-    else
+    else if (_is_cli_command(call))
       execute(args)
+    else
+      executeDirect(args)
   }
 
   def repl(call: OperationCall) {
@@ -58,11 +59,89 @@ class Cozy(
   def createHttpHandle(): HttpHandle = {
     val args = Array[String]()
     val kal = interpreter
+    val call = _operation_call(args)
+    kal.http(call)
+  }
+
+  def executeDirect(args: Array[String]): Unit = {
+    _to_repl_commandline(args) match {
+      case Some(s) =>
+        val c = _operation_call(Array(s))
+        interpreter.execute(c)
+      case None =>
+        execute(args)
+    }
+  }
+
+  private def _is_cli_command(call: OperationCall): Boolean =
+    call.argumentsAsString.headOption.fold(false)(_is_cli_command)
+
+  private def _is_cli_command(name: String): Boolean =
+    _engine.commandParser(name) match {
+      case _: CommandParser.Found[_] => true
+      case _: CommandParser.Candidates[_] => true
+      case _: CommandParser.NotFound[_] => false
+    }
+
+  private def _operation_call(args: Array[String]): OperationCall = {
     val req = spec.Request.empty
     val res = spec.Response()
     val op = spec.Operation("cozy", req, res)
-    val call = OperationCall.create(op, args)
-    kal.http(call)
+    val request = Request.create(op, args)
+    OperationCall(environment, op, request, Response())
+  }
+
+  private def _to_repl_commandline(args: Array[String]): Option[String] =
+    _leading_command(args).map { case (command, rest) =>
+      val converted = _convert_args(rest)
+      (Vector(command) ++ converted).mkString(" ")
+    }
+
+  private def _leading_command(args: Array[String]): Option[(String, List[String])] = {
+    val i = args.indexWhere(x => !x.startsWith("-"))
+    if (i >= 0)
+      Some((args(i), args.drop(i + 1).toList))
+    else
+      None
+  }
+
+  private def _convert_args(args: List[String]): Vector[String] = {
+    @annotation.tailrec
+    def go(xs: List[String], z: Vector[String]): Vector[String] = xs match {
+      case Nil => z
+      case x :: xx if x.startsWith(":") =>
+        go(xx, z :+ x)
+      case x :: xx if x.startsWith("--") =>
+        _split_option(x.drop(2)) match {
+          case Some((name, value)) =>
+            go(xx, z :+ s":$name" :+ _quote(value))
+          case None =>
+            xx match {
+              case y :: yy if !y.startsWith("-") =>
+                go(yy, z :+ s":${x.drop(2)}" :+ _quote(y))
+              case _ =>
+                go(xx, z :+ s":${x.drop(2)}")
+            }
+        }
+      case x :: xx =>
+        go(xx, z :+ _quote(x))
+    }
+    go(args, Vector.empty)
+  }
+
+  private def _split_option(p: String): Option[(String, String)] = {
+    val i = p.indexOf('=')
+    if (i >= 0)
+      Some((p.substring(0, i), p.substring(i + 1)))
+    else
+      None
+  }
+
+  private def _quote(p: String): String = {
+    val escaped = p.
+      replace("\\", "\\\\").
+      replace("\"", "\\\"")
+    s""""$escaped""""
   }
 }
 
